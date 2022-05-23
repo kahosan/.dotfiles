aba94dac2443ec1dda4cd8903c309f4930e6cb30
kahosan <15116355657l@gmail.com>
2022-05-23 14:58:00 +0800

    feat: update "nvim-tree" config

--- .config/nvim/lua/modules/ui/config.lua
+++ .config/nvim/lua/modules/ui/config.lua
@@ -198,45 +198,106 @@ function config.nvim_gps()
 end
 
 function config.nvim_tree()
-  require("nvim-tree").setup {
-    -- 关闭文件时自动关闭
-    -- project plugin 需要这样设置
-    update_cwd = true,
-    update_focused_file = {
-      enable = true,
-      update_cwd = true,
-    },
-    -- auto_close = true,
-    actions = {
-      open_file = {
-        -- 首次打开大小适配
-        resize_window = true,
-        -- 打开文件时关闭
-        quit_on_open = false,
-      },
-    },
-    -- 不显示 git 状态图标
-    git = {
-      enable = true
-    },
-    system_open = {
-      cmd = 'open', -- mac 直接设置为 open
-    },
-    view = {
-      side = 'left'
-    },
-    -- 不显示行数
-    -- number = false,
-    -- relativenumber = false,
-    -- -- 显示图标
-    -- signcolumn = 'yes',
-  }
+	vim.g.nvim_tree_root_folder_modifier = ":e"
+	vim.g.nvim_tree_icon_padding = " "
+	vim.g.nvim_tree_symlink_arror = "  "
+	vim.g.nvim_tree_respect_buf_cwd = 1
 
-  -- 自动关闭
-  vim.cmd([[
-    autocmd BufEnter * ++nested if winnr('$') == 1 && bufname() == 'NvimTree_' . tabpagenr() | quit | endif
-  ]])
+	vim.g.nvim_tree_icons = {
+		["default"] = "", --
+		["symlink"] = "",
+		["git"] = {
+			["unstaged"] = "",
+			["staged"] = "", --
+			["unmerged"] = "שׂ",
+			["renamed"] = "", --
+			["untracked"] = "ﲉ",
+			["deleted"] = "",
+			["ignored"] = "", --◌
+		},
+		["folder"] = {
+			-- ['arrow_open'] = "",
+			-- ['arrow_closed'] = "",
+			["arrow_open"] = "",
+			["arrow_closed"] = "",
+			["default"] = "",
+			["open"] = "",
+			["empty"] = "",
+			["empty_open"] = "",
+			["symlink"] = "",
+			["symlink_open"] = "",
+		},
+	}
+
+	require("nvim-tree").setup({
+		auto_reload_on_write = true,
+		disable_netrw = false,
+		hijack_cursor = true,
+		hijack_netrw = true,
+		hijack_unnamed_buffer_when_opening = false,
+		ignore_buffer_on_setup = false,
+		open_on_setup = false,
+		open_on_setup_file = false,
+		open_on_tab = false,
+		sort_by = "name",
+		update_cwd = true,
+		view = {
+			width = 30,
+			height = 30,
+			side = "left",
+			preserve_window_proportions = false,
+			number = false,
+			relativenumber = false,
+			signcolumn = "yes",
+			hide_root_folder = false,
+		},
+		renderer = {
+			indent_markers = {
+				enable = true,
+				icons = {
+					corner = "└ ",
+					edge = "│ ",
+					none = "  ",
+				},
+			},
+		},
+		hijack_directories = {
+			enable = true,
+			auto_open = true,
+		},
+		update_focused_file = {
+			enable = true,
+			update_cwd = true,
+			ignore_list = {},
+		},
+		ignore_ft_on_setup = {},
+		filters = {
+			dotfiles = false,
+			custom = { ".DS_Store" },
+			exclude = {},
+		},
+		actions = {
+			use_system_clipboard = true,
+			change_dir = {
+				enable = true,
+				global = false,
+			},
+			open_file = {
+				quit_on_open = false,
+				resize_window = false,
+				window_picker = {
+					enable = true,
+					chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890",
+					exclude = {
+						filetype = { "notify", "packer", "qf", "diff", "fugitive", "fugitiveblame" },
+						buftype = { "nofile", "terminal", "help" },
+					},
+				},
+			},
+		},
+	})
 end
+
 
 function config.nvim_bufferline()
   require("bufferline").setup {
