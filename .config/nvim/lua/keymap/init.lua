9186b3043fbbce41706368d8880172809b301956
kahosan <15116355657l@gmail.com>
2022-05-23 17:15:30 +0800

    feat: add FTerm key bindings

--- .config/nvim/lua/keymap/init.lua
+++ .config/nvim/lua/keymap/init.lua
@@ -59,8 +59,11 @@ local plug_map = {
 	["n|<leader>c"] = map_cu("HopChar1"):with_noremap(),
 	["n|<leader>cc"] = map_cu("HopChar2"):with_noremap(),
 
-	-- ToggleTerm
-	["n|<C-\\>"] = map_cr("ToggleTerm dir='%:p:h'"):with_noremap():with_silent()
+	-- Fterm
+	["n|<leader>ft"] = map_cu('lua require("FTerm").toggle()'):with_noremap():with_silent(),
+	-- ["t|<leader>fd"] = map_cu([[<C-\><C-n><CMD>lua require("FTerm").toggle()]]):with_noremap():with_silent(),
+	["t|<leader>fd"] = map_cu([[<C-\><C-n><CMD>lua require("FTerm").exit()]]):with_noremap():with_silent(),
+	["n|<Leader>g"] = map_cu("lua require('FTerm').run('lazygit')"):with_noremap():with_silent(),
 }
 
 bind.nvim_load_mapping(plug_map)
