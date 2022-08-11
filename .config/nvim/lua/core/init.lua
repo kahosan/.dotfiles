4becb0294f22eed0948f4b67e44950780457a4cc
kahosan <15116355657l@gmail.com>
2022-08-11 11:42:55 +0800

    chore: add neovide_config

--- .config/nvim/lua/core/init.lua
+++ .config/nvim/lua/core/init.lua
@@ -50,6 +50,19 @@ local leader_map = function()
   vim.g.mapleader = ";"
 end
 
+local neovide_config = function()
+	vim.g.neovide_refresh_rate = 120
+	vim.g.neovide_cursor_vfx_mode = "railgun"
+	vim.g.neovide_no_idle = true
+	vim.g.neovide_cursor_animation_length = 0.03
+	vim.g.neovide_cursor_trail_length = 0.05
+	vim.g.neovide_cursor_antialiasing = true
+	vim.g.neovide_cursor_vfx_opacity = 200.0
+	vim.g.neovide_cursor_vfx_particle_lifetime = 1.2
+	vim.g.neovide_cursor_vfx_particle_speed = 20.0
+	vim.g.neovide_cursor_vfx_particle_density = 5.0
+end
+
 local load_core = function()
   local pack = require("core.pack")
   createdir()
@@ -57,6 +70,7 @@ local load_core = function()
   leader_map()
 
   pack.ensure_plugins()
+  neovide_config()
   -- clipboard_config()
 
   require("core.options")
