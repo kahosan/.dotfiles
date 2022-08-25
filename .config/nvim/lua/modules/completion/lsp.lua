778fe4bf49d9a9d1cda4f9d96ac32114f2362b92
kahosan <15116355657l@gmail.com>
2022-08-25 13:41:37 +0800

    fix(clangd[lsp]): Args aren't being passed correctly.

--- .config/nvim/lua/modules/completion/lsp.lua
+++ .config/nvim/lua/modules/completion/lsp.lua
@@ -107,13 +107,19 @@ for _, server in ipairs(mason_lsp.get_installed_servers()) do
       capabilities = copy_capabilities,
       single_file_support = true,
       on_attach = custom_attach,
-      args = {
-        "--background-index",
-        "-std=c++20",
-        "--pch-storage=memory",
-        "--clang-tidy",
-        "--suggest-missing-includes",
-      },
+      cmd = {
+				"clangd",
+				"--background-index",
+				"--pch-storage=memory",
+				-- You MUST set this arg ↓ to your clangd executable location (if not included)!
+				"--query-driver=/usr/bin/clang++,/usr/bin/**/clang-*,/bin/clang,/bin/clang++,/usr/bin/gcc,/usr/bin/g++",
+				"--clang-tidy",
+				"--all-scopes-completion",
+				"--cross-file-rename",
+				"--completion-style=detailed",
+				"--header-insertion-decorators",
+				"--header-insertion=iwyu",
+			},
       commands = {
         ClangdSwitchSourceHeader = {
           function()
