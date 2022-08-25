9daac69f40ebfe234cf2f8afce5e290089943b77
kahosan <15116355657l@gmail.com>
2022-08-25 14:08:12 +0800

    fix: remove treesitter for code fold

--- .config/nvim/lua/modules/editor/config.lua
+++ .config/nvim/lua/modules/editor/config.lua
@@ -35,8 +35,8 @@ function config.illuminate()
 end
 
 function config.nvim_treesitter()
-  -- vim.api.nvim_command("set foldmethod=expr")
-  vim.api.nvim_command("set foldexpr=nvim_treesitter#foldexpr()")
+  vim.api.nvim_command("set foldmethod=expr")
+	vim.api.nvim_command("set foldexpr=nvim_treesitter#foldexpr()")
 
   require("nvim-treesitter.configs").setup({
     ensure_installed = {
