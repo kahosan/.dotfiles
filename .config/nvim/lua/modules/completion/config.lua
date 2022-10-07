5d342f56549923fe756f73c9af2bee82538bfdfc
kahosan <15116355657l@gmail.com>
2022-10-07 20:39:46 +0800

    chore(BREAKING CHANGE): use <Tab> and <S-Tab> to navigate in snippets.

--- .config/nvim/lua/modules/completion/config.lua
+++ .config/nvim/lua/modules/completion/config.lua
@@ -9,6 +9,11 @@ function config.cmp()
     return vim.api.nvim_replace_termcodes(str, true, true, true)
   end
 
+  local has_words_before = function()
+    local line, col = table.unpack(vim.api.nvim_win_get_cursor(0))
+    return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+  end
+
   local cmp = require("cmp")
   cmp.setup({
     sorting = {
@@ -78,21 +83,40 @@ function config.cmp()
       ["<C-d>"] = cmp.mapping.scroll_docs(-4),
       ["<C-f>"] = cmp.mapping.scroll_docs(4),
       ["<C-e>"] = cmp.mapping.close(),
-      ["<C-h>"] = function(fallback)
-        if require("luasnip").jumpable(-1) then
+      ["<Tab>"] = cmp.mapping(function(fallback)
+        if cmp.visible() then
+          cmp.select_next_item()
+        elseif require("luasnip").expand_or_jumpable() then
+          vim.fn.feedkeys(t("<Plug>luasnip-expand-or-jump"), "")
+        elseif has_words_before() then
+          cmp.complete()
+        else
+          fallback()
+        end
+      end, { "i", "s" }),
+      ["<S-Tab>"] = cmp.mapping(function(fallback)
+        if cmp.visible() then
+          cmp.select_prev_item()
+        elseif require("luasnip").jumpable(-1) then
           vim.fn.feedkeys(t("<Plug>luasnip-jump-prev"), "")
         else
           fallback()
         end
-      end,
-      ["<C-l>"] = function(fallback)
-        if require("luasnip").expand_or_jumpable() then
-          vim.fn.feedkeys(t("<Plug>luasnip-expand-or-jump"), "")
-        else
-          fallback()
-        end
-      end,
+      end, { "i", "s" }),
     }),
+    --   if require("luasnip").jumpable(-1) then
+    --     vim.fn.feedkeys(t("<Plug>luasnip-jump-prev"), "")
+    --   else
+    --     fallback()
+    --   end
+    -- end,
+    -- ["<C-l>"] = function(fallback)
+    --   if require("luasnip").expand_or_jumpable() then
+    --     vim.fn.feedkeys(t("<Plug>luasnip-expand-or-jump"), "")
+    --   else
+    --     fallback()
+    --   end
+    -- end,
     snippet = {
       expand = function(args)
         require("luasnip").lsp_expand(args.body)
