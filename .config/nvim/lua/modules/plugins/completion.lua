2d423c98f762edb97be2d2c28794f1855f217fa9
kahosan <15116355657l@gmail.com>
2023-02-15 00:06:17 +0800

    chore: remove snip

--- .config/nvim/lua/modules/plugins/completion.lua
+++ .config/nvim/lua/modules/plugins/completion.lua
@@ -1,67 +1,67 @@
 local completion = {}
 
 completion["jose-elias-alvarez/null-ls.nvim"] = {
-	lazy = false,
+  lazy = false,
   config = require("completion.null-ls"),
 }
 completion["jay-babu/mason-null-ls.nvim"] = {
-	lazy = false,
-	config = require('completion.mason-null-ls')
+  lazy = false,
+  config = require('completion.mason-null-ls')
 }
 completion["neovim/nvim-lspconfig"] = {
-	lazy = true,
-	event = { "BufReadPost", "BufAdd", "BufNewFile" },
-	config = require("completion.lsp"),
-	dependencies = {
-		{ "williamboman/mason.nvim" },
-		{ "williamboman/mason-lspconfig.nvim" },
-		{
-			"WhoIsSethDaniel/mason-tool-installer.nvim",
-			config = require("completion.mason-tool-installer"),
-		},
-		{
-			"glepnir/lspsaga.nvim",
-			config = require("completion.lspsaga"),
-		},
-		{ "ray-x/lsp_signature.nvim" },
-	},
+  lazy = true,
+  event = { "BufReadPost", "BufAdd", "BufNewFile" },
+  config = require("completion.lsp"),
+  dependencies = {
+    { "williamboman/mason.nvim" },
+    { "williamboman/mason-lspconfig.nvim" },
+    {
+      "WhoIsSethDaniel/mason-tool-installer.nvim",
+      config = require("completion.mason-tool-installer"),
+    },
+    {
+      "glepnir/lspsaga.nvim",
+      config = require("completion.lspsaga"),
+    },
+    { "ray-x/lsp_signature.nvim" },
+  },
 }
 completion["hrsh7th/nvim-cmp"] = {
-	event = "InsertEnter",
-	config = require("completion.cmp"),
-	dependencies = {
-		{
-			"L3MON4D3/LuaSnip",
-			dependencies = { "rafamadriz/friendly-snippets" },
-			config = require("completion.luasnip"),
-		},
-		{ "onsails/lspkind.nvim" },
-		{ "lukas-reineke/cmp-under-comparator" },
-		{ "saadparwaiz1/cmp_luasnip" },
-		{ "hrsh7th/cmp-nvim-lsp" },
-		{ "hrsh7th/cmp-nvim-lua" },
-		{ "andersevenrud/cmp-tmux" },
-		{ "hrsh7th/cmp-path" },
-		{ "f3fora/cmp-spell" },
-		{ "hrsh7th/cmp-buffer" },
-		{ "kdheepak/cmp-latex-symbols" },
-		{
-			"windwp/nvim-autopairs",
-			config = require("completion.autopairs"),
-		},
-		-- { "tzachar/cmp-tabnine", build = "./install.sh", config = require("completion.tabnine") },
-	},
+  event = "InsertEnter",
+  config = require("completion.cmp"),
+  dependencies = {
+    -- {
+    --  "L3MON4D3/LuaSnip",
+    --  dependencies = { "rafamadriz/friendly-snippets" },
+    --  config = require("completion.luasnip"),
+    -- },
+    { "onsails/lspkind.nvim" },
+    { "lukas-reineke/cmp-under-comparator" },
+    { "saadparwaiz1/cmp_luasnip" },
+    { "hrsh7th/cmp-nvim-lsp" },
+    { "hrsh7th/cmp-nvim-lua" },
+    { "andersevenrud/cmp-tmux" },
+    { "hrsh7th/cmp-path" },
+    { "f3fora/cmp-spell" },
+    { "hrsh7th/cmp-buffer" },
+    { "kdheepak/cmp-latex-symbols" },
+    {
+      "windwp/nvim-autopairs",
+      config = require("completion.autopairs"),
+    },
+    -- { "tzachar/cmp-tabnine", build = "./install.sh", config = require("completion.tabnine") },
+  },
 }
 completion["zbirenbaum/copilot.lua"] = {
-	cmd = "Copilot",
-	event = "InsertEnter",
-	config = require("completion.copilot"),
-	dependencies = {
-		{
-			"zbirenbaum/copilot-cmp",
-			config = require("completion.copilot-cmp"),
-		},
-	},
+  cmd = "Copilot",
+  event = "InsertEnter",
+  config = require("completion.copilot"),
+  dependencies = {
+    {
+      "zbirenbaum/copilot-cmp",
+      config = require("completion.copilot-cmp"),
+    },
+  },
 }
 
 return completion
