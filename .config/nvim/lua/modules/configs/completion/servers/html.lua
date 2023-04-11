f365e61ab69a9632bf9e0bb2ad80bd7591958215
kahosan <15116355657l@gmail.com>
2023-04-11 14:22:06 +0800

    nvim: update

--- .config/nvim/lua/modules/configs/completion/servers/html.lua
+++ .config/nvim/lua/modules/configs/completion/servers/html.lua
@@ -1,12 +1,12 @@
--- https://github.com/vscode-langservers/vscode-html-languageserver-bin
+-- https://github.com/hrsh7th/vscode-langservers-extracted
 return {
-	cmd = { "html-languageserver", "--stdio" },
-	filetypes = { "html" },
-	init_options = {
-		configurationSection = { "html", "css", "javascript" },
-		embeddedLanguages = { css = true, javascript = true },
-	},
-	settings = {},
-	single_file_support = true,
-	flags = { debounce_text_changes = 500 },
+  cmd = { "vscode-html-language-server", "--stdio" },
+  filetypes = { "html" },
+  init_options = {
+    configurationSection = { "html", "css", "javascript" },
+    embeddedLanguages = { css = true, javascript = true },
+  },
+  settings = {},
+  single_file_support = true,
+  flags = { debounce_text_changes = 500 },
 }
