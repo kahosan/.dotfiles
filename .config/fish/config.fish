b5ecd9e1c1e56d1f831668ad7d947aa932a4cda4
kahosan <15116355657l@gmail.com>
2023-05-20 23:56:16 +0800

    fix: `/opt/homebrew/bin`

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -15,6 +15,7 @@ fish_add_path ~/.cargo/bin
 fish_add_path (go env GOPATH)/bin
 fish_add_path ~/.pnpm-global/bin
 fish_add_path /opt/homebrew/sbin
+fish_add_path /opt/homebrew/bin
 
 # ni
 set -x NI_CONFIG_FILE ~/.config/ni/nirc
