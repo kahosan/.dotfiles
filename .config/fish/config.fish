1e2b26e3b965da386b4fdbd0f4562f6d3341af5c
kahosan <15116355657l@gmail.com>
2024-11-27 12:41:48 +0800

    fix(fish): path

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -38,7 +38,7 @@ fish_add_path ~/.cargo/bin
 fish_add_path ~/.pnpm-global/bin
 
 if test (uname) = "Darwin"
-  fish_add_path $(brew --prefix python@3.11)/libexec/bin
+  fish_add_path (brew --prefix python@3.11)/libexec/bin
   fish_add_path /opt/homebrew/bin
   fish_add_path /opt/homebrew/sbin
   fish_add_path /opt/homebrew/opt/libpq/bin
