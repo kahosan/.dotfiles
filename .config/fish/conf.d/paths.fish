a9bb136cd3cde616041c1e1fe3c49dfb29dbbb25
kahosan <15116355657l@gmail.com>
2025-03-26 14:18:45 +0800

    chore(fish): update paths

--- .config/fish/conf.d/paths.fish
+++ .config/fish/conf.d/paths.fish
@@ -1,4 +1,3 @@
-fish_add_path /usr/local/sbin
 fish_add_path ~/.local/bin
 fish_add_path ~/.cargo/bin
 fish_add_path ~/.pnpm-global/bin
@@ -6,14 +5,10 @@ fish_add_path ~/.pnpm-global/bin
 if test (uname) = Darwin
     fish_add_path /opt/homebrew/bin
     fish_add_path /opt/homebrew/sbin
-    fish_add_path (brew --prefix python@3.11)/libexec/bin
     fish_add_path /opt/homebrew/opt/libpq/bin
 end
 
 if test (uname) = Linux
-    fish_add_path ~/.local/go/bin
-    fish_add_path ~/.local/share/pnpm
-    fish_add_path ~/.local/share/fnm
 end
 
 if type -q go
