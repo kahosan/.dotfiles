2d52f36b3251ab91471bdd55bb77b3dc77aba4ae
kahosan <15116355657l@gmail.com>
2025-04-19 17:01:24 +0800

    chore(setup): s

--- setup.sh
+++ setup.sh
@@ -1,30 +1,39 @@
 #!/bin/bash
 set -e
 
+packages="cmake git wget curl tmux unzip fish"
+read -rp "have you installed ${packages} yet? [y/N] " confirm
+if [[ "$confirm" != "y" ]]; then
+	echo "please install the packages first"
+	exit 1
+fi
+
 require() {
 	command -v "${1}" &>/dev/null && return 0
-	printf 'missing required application: %s\n' "${1}" >&2
+	read -rp "do you need to install ${1}? [y/N] " confirm
+	if [[ "$confirm" != "y" ]]; then
+		return 0
+	fi
 	return 1
 }
 
 if ! require brew; then
-	/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+	bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 fi
 
 if ! require starship; then
-	if ! curl -fsSL https://starship.rs/install.sh | sh; then
-		echo -e "\nfailed to install Startship"
-		exit 1
-	fi
+	curl -fsSL https://starship.rs/install.sh | sh
 fi
 
+echo ""
+
 if [[ ! -d "$HOME/.local/bin" ]]; then
-	echo -e "mkdir .local/bin folder\n"
+	echo "mkdir .local/bin folder"
 	mkdir -p "$HOME/.local/bin"
 fi
 
 if [[ ! -d "$HOME/.ssh" ]]; then
-	echo -e "mkdir .ssh folder\n"
+	echo "mkdir .ssh folder"
 	mkdir "$HOME/.ssh"
 	touch "$HOME/.ssh/authorized_keys"
 	chmod 600 "$HOME/.ssh"
@@ -32,18 +41,11 @@ if [[ ! -d "$HOME/.ssh" ]]; then
 fi
 
 if [[ -d "$HOME/.config/fish" ]]; then
-	echo -e "mv old fish folder to fish.bak\n"
+	echo "mv old fish folder to fish.bak"
 	mv "$HOME/.config/fish" "$HOME/.config/fish.bak"
 fi
 
-packages="cmake git wget curl tmux unzip rar fish"
-echo "need packages: $packages"
-read -rp "have you installed the package yet? [y/N] " confirm
-echo -e "\n"
-if [[ "$confirm" != "y" ]]; then
-	echo -e "\nplease install the packages first"
-	exit 1
-fi
+echo ""
 
 # link dotfile to ~/.config
 
@@ -62,7 +64,8 @@ for item in "$SOURCE_DIR"/*; do
 		echo -e "Created symlink: $target_path -> $item"
 	fi
 done
-echo -e "\n"
+
+echo ""
 
 ln -s ~/.dotfiles/scripts ~/.config/scripts
 ln -s ~/.dotfiles/.tmux.conf ~/.tmux.conf
@@ -72,13 +75,13 @@ ln -s ~/.dotfiles/.gitconfig ~/.gitconfig
 read -rp "need to configure git? [y/N] " confirm
 if [[ "$confirm" == "y" ]]; then
 	git_email="$(git config user.email)"
-	echo -e "generating new SSH key for $git_email ...\n"
+	echo "generating new SSH key for $git_email ..."
 	if ! ssh-keygen -t ed25519 -C "$git_email"; then
 		echo "failed to generate SSH key"
 		exit 1
 	fi
 
-	echo -e "starting ssh-agent in the background ...\n"
+	echo "starting ssh-agent in the background ..."
 	if ! eval "$(ssh-agent -s)"; then
 		echo "failed to start ssh-agent"
 		exit 1
@@ -89,7 +92,7 @@ if [[ "$confirm" == "y" ]]; then
 		exit 1
 	fi
 
-	echo -e "copying public key to clipboard, please paste it into your GitHub account settings ...\n"
+	echo "copying public key to clipboard, please paste it into your GitHub account settings ..."
 	if ! cat ~/.ssh/id_ed25519.pub; then
 		echo "failed to copy public key to clipboard"
 		exit 1
