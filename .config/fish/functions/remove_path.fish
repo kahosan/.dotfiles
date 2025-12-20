18be6ad34845842cca217116be5918653c9fc2aa
kahosan <15116355657l@gmail.com>
2025-12-20 12:30:11 +0800

    chore(fish): remove path

--- .config/fish/functions/remove_path.fish
+++ .config/fish/functions/remove_path.fish
@@ -1,6 +1,8 @@
 function remove_path
-    if set -l index (contains -i "$argv" $fish_user_paths)
+    if set -l index (contains -i "$argv[1]" $fish_user_paths)
         set -e fish_user_paths[$index]
-        echo "Removed $argv from the path"
+        echo "removed $argv[1] from the path"
+    else
+        echo "$argv[1] not found in fish_user_paths"
     end
 end
