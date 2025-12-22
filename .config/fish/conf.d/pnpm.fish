1632868aacc66274b2a4b8d141f615606eee445c
kahosan <15116355657l@gmail.com>
2025-12-22 17:14:47 +0800

    fix(fish): pnpm env

--- .config/fish/conf.d/pnpm.fish
+++ .config/fish/conf.d/pnpm.fish
@@ -1,4 +1,2 @@
-if type -q pnpm
-    set -x PNPM_HOME "$HOME/.local/share/pnpm"
-    fish_add_path "$HOME/.local/share/pnpm"
-end
+set -x PNPM_HOME "$HOME/.local/share/pnpm"
+fish_add_path "$HOME/.local/share/pnpm"
