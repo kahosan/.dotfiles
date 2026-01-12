53ef001577121d143885677d153dd3f1d76c312f
kahosan <15116355657l@gmail.com>
2026-01-12 11:26:55 +0800

    chore(fish): add `XDG_STATE_HOME` env

--- .config/fish/conf.d/env.fish
+++ .config/fish/conf.d/env.fish
@@ -1,6 +1,7 @@
 set -gx XDG_CONFIG_HOME $HOME/.config
 set -gx XDG_DATA_HOME $HOME/.local/share
 set -gx XDG_CACHE_HOME $HOME/.cache
+set -gx XDG_STATE_HOME $HOME/.local/state
 
 set -gx GOPATH $XDG_DATA_HOME/go
 set -gx PNPM_HOME $XDG_DATA_HOME/pnpm
